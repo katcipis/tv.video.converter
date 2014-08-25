d58f08f3cd5ede6cdef4286cc2bca4aafe769d92
Tiago Katcipis <tiagokatcipis@gmail.com>
2014-08-25 20:41:58 -0300

    one more step :D

--- tv-video-batch-converter.py
+++ tv-video-batch-converter.py
@@ -1,13 +1,33 @@
 #! /usr/bin/env python
 
+import sys
+import os
+import glob
+
 def getSourceDir():
-    pass
+    return os.path.expanduser(sys.argv[1]);
+
+def getFilesWithSuffix (directory, suffix):
+    return glob.glob(sourceDir + "/*." + suffix);
 
 def getSourceVideos(sourceDir):
-    return [];
+    videos = [];
+    subtitleSuffix = "srt";
+    videos.extend(getFilesWithSuffix(sourceDir, "mkv"));
+    videos.extend(getFilesWithSuffix(sourceDir, "mp4"));
+    videos.extend(getFilesWithSuffix(sourceDir, "avi"));
+    subtitles = getFilesWithSuffix(sourceDir, subtitleSuffix);
+    result = [];
+
+    for video in videos:
+        expectedSubtitle = video[:-3] + subtitleSuffix;
+        if expectedSubtitle in subtitles:
+            result.append({ "video" : video, "subtitle" : expectedSubtitle});
+
+    return result;
 
 def getSinkDir():
-    pass
+    return os.path.expanduser(sys.argv[2]);
 
 def askUserToGoOn():
     pass
@@ -16,7 +36,13 @@ def generateSinkVideo(sourceVideo, sinkDir):
     pass
 
 def printDescriptionOfVideos(sourceVideos):
-    pass
+    for videoInfo in sourceVideos:
+        print("\nVideo: {video}\nSubtitle: {subtitle}\n".format(**videoInfo));
+
+
+if len(sys.argv) < 3:
+    print("usage: {0[0]} <source dir of videos to transcode> <sink dir where transcoded videos will be saved>".format(sys.argv));
+    exit();
 
 
 sourceDir = getSourceDir();
