c984d8f3ac486ef645be14a5220122678d17e31f
Tiago Katcipis <tiagokatcipis@gmail.com>
2014-08-25 21:17:08 -0300

    it is done :D

--- tv-video-batch-converter.py
+++ tv-video-batch-converter.py
@@ -3,6 +3,8 @@
 import sys
 import os
 import glob
+import subprocess
+
 
 def getSourceDir():
     return os.path.expanduser(sys.argv[1]);
@@ -30,9 +32,19 @@ def getSinkDir():
     return os.path.expanduser(sys.argv[2]);
 
 def askUserToGoOn():
-    pass
+    proceed = raw_input("Do you want to go on ? this will probably take a lot of time (y/N): ");
+    if not 'y' in proceed.lower():
+        print("User requested to abort");
+        exit();
+    print("Going on to process a bunch of videos");
 
 def generateSinkVideo(sourceVideo, sinkDir):
+    args = ["tv-video-converter"];
+    sinkVideo = os.path.basename(sourceVideo["video"])[:-3] + "mp4";
+    args.append(sourceVideo["video"]);
+    args.append(sourceVideo["subtitle"]);
+    args.append(os.path.join(sinkDir, sinkVideo));
+    subprocess.call(args);
     pass
 
 def printDescriptionOfVideos(sourceVideos):
@@ -53,7 +65,7 @@ sinkDir = getSinkDir();
 printDescriptionOfVideos(sourceVideos);
 askUserToGoOn();
 
-print("Starting the transcoding process, this may take a while");
+print("Starting the transcoding process, this may take a while. All videos will be saved on: {0}".format(sinkDir));
 
 for sourceVideo in sourceVideos:
     generateSinkVideo(sourceVideo, sinkDir);
--- tv-video-converter.sh
+++ tv-video-converter.sh
@@ -8,4 +8,4 @@ echo ""
 
 echo "Video filepath: "$1" Subtitle filepath: "$2" Converted video filepath: "$3
 
-gst-launch-1.0 filesrc location=$1 ! decodebin name=decoder ! queue ! videoconvert ! videoscale ! video/x-raw,width=1920,height=1080 ! subtitleoverlay font-desc="DejaVu Sans 25px" name=subtitle ! x264enc ! mp4mux name=muxer ! filesink location=$3 decoder. ! audioconvert ! audioresample ! queue ! lamemp3enc ! muxer. filesrc location=$2 ! queue ! subparse ! subtitle.subtitle_sink
+gst-launch-1.0 filesrc location="$1" ! decodebin name=decoder ! queue ! videoconvert ! videoscale ! video/x-raw,width=1920,height=1080 ! subtitleoverlay font-desc="DejaVu Sans 25px" name=subtitle ! x264enc ! mp4mux name=muxer ! filesink location="$3" decoder. ! audioconvert ! audioresample ! queue ! lamemp3enc ! muxer. filesrc location="$2" ! queue ! subparse ! subtitle.subtitle_sink
