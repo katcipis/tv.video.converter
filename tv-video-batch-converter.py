6bdbead705ff979517aa25f2c30feafc40aee677
Tiago Katcipis <tiagokatcipis@gmail.com>
2014-09-14 15:22:28 -0300

    just some small improvements.

--- tv-video-batch-converter.py
+++ tv-video-batch-converter.py
@@ -9,27 +9,31 @@ import subprocess
 def getSourceDir():
     return os.path.expanduser(sys.argv[1]);
 
+
 def getFilesWithSuffix (directory, suffix):
     return glob.glob(sourceDir + "/*." + suffix);
 
+
 def getSourceVideos(sourceDir):
     videos = [];
-    subtitleSuffix = "srt";
     videos.extend(getFilesWithSuffix(sourceDir, "mkv"));
     videos.extend(getFilesWithSuffix(sourceDir, "mp4"));
     videos.extend(getFilesWithSuffix(sourceDir, "avi"));
-    subtitles = getFilesWithSuffix(sourceDir, subtitleSuffix);
     result = [];
 
     for video in videos:
-        expectedSubtitle = video[:-3] + subtitleSuffix;
-        if expectedSubtitle in subtitles:
+        expectedSubtitle = video[:-3] + "srt";
+        if os.path.exists(expectedSubtitle):
             result.append({ "video" : video, "subtitle" : expectedSubtitle});
+        else:
+            print("Ignoring file: [{0}] since it does not have a subtitle".format(video));
 
     return result;
 
+
 def getSinkDir():
     return os.path.expanduser(sys.argv[2]);
+
 
 def askUserToGoOn():
     proceed = raw_input("Do you want to go on ? this will probably take a lot of time (y/N): ");
@@ -38,6 +42,7 @@ def askUserToGoOn():
         exit();
     print("Going on to process a bunch of videos");
 
+
 def generateSinkVideo(sourceVideo, sinkDir):
     args = ["tv-video-converter"];
     sinkVideo = os.path.basename(sourceVideo["video"])[:-3] + "mp4";
@@ -45,9 +50,10 @@ def generateSinkVideo(sourceVideo, sinkDir):
     args.append(sourceVideo["subtitle"]);
     args.append(os.path.join(sinkDir, sinkVideo));
     subprocess.call(args);
-    pass
+
 
 def printDescriptionOfVideos(sourceVideos):
+    print("\n\nThe following videos have been detected: \n");
     for videoInfo in sourceVideos:
         print("\nVideo: {video}\nSubtitle: {subtitle}\n".format(**videoInfo));
 
@@ -59,6 +65,9 @@ if len(sys.argv) < 3:
 
 sourceDir = getSourceDir();
 sourceVideos = getSourceVideos(sourceDir);
+if not sourceVideos:
+    print("No video found at sourceDir: [{0}]".format(sourceDir));
+    exit();
 
 sinkDir = getSinkDir();
 
