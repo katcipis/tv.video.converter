5e2ac86b46f34483716dd7abcef8f1183d9bdb29
Tiago <tiagokatcipis@gmail.com>
2014-11-30 15:39:04 -0200

    we need support for it to work without subtitles.

--- tv-video-converter.sh
+++ tv-video-converter.sh
@@ -5,11 +5,25 @@ echo "It will transcode videos to a more usual TV friendly format and also overl
 echo "The format will be a mp4 container with H.264 for video and acc for sound"
 echo ""
 
-if [ "$#" -ne 3 ]; then
-    echo "usage: $0 <original video filepath> <subtitle filepath> <converted video filepath>"
-    exit 1
+echo "usage: $0 <original video filepath> <converted video filepath> <subtitle path (optional)>"
+echo ""
+
+input_path=$1
+output_path=$2
+subtitle_path=$3
+
+if [ "$#" -eq 2 ]; then
+
+    echo "Video filepath: "$input_path" Converted video filepath: "$output_path
+
+    gst-launch-1.0 filesrc location="$input_path" ! decodebin name=decoder ! queue ! videoconvert ! videoscale ! video/x-raw,width=1920,height=1080 ! x264enc ! qtmux name=muxer ! filesink location="$output_path" decoder. ! audioconvert ! audioresample ! queue ! voaacenc ! muxer.
+
 fi
 
-echo "Video filepath: "$1" Subtitle filepath: "$2" Converted video filepath: "$3
+if [ "$#" -eq 3 ]; then
 
-gst-launch-1.0 filesrc location="$1" ! decodebin name=decoder ! queue ! videoconvert ! videoscale ! video/x-raw,width=1920,height=1080 ! subtitleoverlay font-desc="DejaVu Sans 25px" name=subtitle ! x264enc ! qtmux name=muxer ! filesink location="$3" decoder. ! audioconvert ! audioresample ! queue ! voaacenc ! muxer. filesrc location="$2" ! queue ! subparse ! subtitle.subtitle_sink
+    echo "Video filepath: "$input_path" Converted video filepath: "$output_path" Subtitle path: "$subtitle_path
+
+    gst-launch-1.0 filesrc location="$input_path" ! decodebin name=decoder ! queue ! videoconvert ! videoscale ! video/x-raw,width=1920,height=1080 ! subtitleoverlay font-desc="DejaVu Sans 25px" name=subtitle ! x264enc ! qtmux name=muxer ! filesink location="$output_path" decoder. ! audioconvert ! audioresample ! queue ! voaacenc ! muxer. filesrc location="$subtitle_path" ! queue ! subparse ! subtitle.subtitle_sink
+
+fi
