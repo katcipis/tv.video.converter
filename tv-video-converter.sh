82f7dbd379a36414473c4175309a6e269826e43a
Tiago Katcipis <tiagokatcipis@gmail.com>
2018-07-13 16:14:22 -0300

    Change CODECs, having problems with h.264

--- tv-video-converter.sh
+++ tv-video-converter.sh
@@ -2,7 +2,7 @@
 
 echo ""
 echo "It will transcode videos to a more usual TV friendly format and also overlay subtitles."
-echo "The format will be a mp4 container with H.264 for video and acc for sound"
+echo "The format will be a webm container with VP8 for video and Vorbis for sound"
 echo ""
 
 echo "usage: $0 <original video filepath> <converted video filepath> <subtitle path (optional)>"
@@ -11,12 +11,15 @@ echo ""
 input_path=$1
 output_path=$2
 subtitle_path=$3
+muxer=webmmux
+videoencoder=vp8enc
+audioencoder=vorbisenc
 
 if [ "$#" -eq 2 ]; then
 
     echo "Video filepath: "$input_path" Converted video filepath: "$output_path
 
-    gst-launch-1.0 filesrc location="$input_path" ! decodebin name=decoder ! queue ! videoconvert ! videoscale ! x264enc ! queue ! qtmux name=muxer ! queue ! filesink location="$output_path" decoder. ! queue ! audioconvert ! audioresample ! avenc_aac ! queue ! muxer.
+    gst-launch-1.0 filesrc location="$input_path" ! decodebin name=decoder ! queue ! videoconvert ! videoscale ! $videoencoder ! queue ! $muxer name=muxer ! queue ! filesink location="$output_path" decoder. ! queue ! audioconvert ! audioresample ! $audioencoder ! queue ! muxer.
 
 fi
 
@@ -24,6 +27,6 @@ if [ "$#" -eq 3 ]; then
 
     echo "Video filepath: "$input_path" Converted video filepath: "$output_path" Subtitle path: "$subtitle_path
 
-    gst-launch-1.0 filesrc location="$input_path" ! decodebin name=decoder ! queue ! videoconvert ! videoscale ! subtitleoverlay font-desc="DejaVu Sans 25px" name=subtitle ! x264enc ! queue ! qtmux name=muxer ! queue ! filesink location="$output_path" decoder. ! queue ! audioconvert ! audioresample ! avenc_aac ! queue ! muxer. filesrc location="$subtitle_path" ! queue ! subparse ! subtitle.subtitle_sink
+    gst-launch-1.0 filesrc location="$input_path" ! decodebin name=decoder ! queue ! videoconvert ! videoscale ! subtitleoverlay font-desc="DejaVu Sans 25px" name=subtitle ! $videoencoder ! queue ! $muxer name=muxer ! queue ! filesink location="$output_path" decoder. ! queue ! audioconvert ! audioresample ! $audioencoder ! queue ! muxer. filesrc location="$subtitle_path" ! queue ! subparse ! subtitle.subtitle_sink
 
 fi
