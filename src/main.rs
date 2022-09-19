732cc51e5293d4ef4b5dff8685bc7e3a382ba407
Tiago Katcipis <tiagokatcipis@gmail.com>
2022-09-19 22:17:48 +0200

    feat: initial structure with scoped threads

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    const NTHREADS: i32 = 4;
+
+    thread::scope(|s| {
+        for i in 0..NTHREADS {
+            s.spawn(move || {
+                println!("starting worker {}", i);
+            });
+        }
+        println!("starting job scheduler");
+    });
+
+    println!("done");
 }
+
