7f2c02d889c89f6a35f8e6d0598e6e92931f6710
Tiago Katcipis <tiagokatcipis@gmail.com>
2022-10-13 23:43:18 +0200

    chore: extract job scheduler

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::thread;
 use std::path::PathBuf;
 use crossbeam_channel;
 use std::env;
+use std::fs;
 
 #[derive(Debug)]
 struct TranscodeJob {
@@ -26,17 +27,19 @@ fn main() {
 
         for i in 0..NTHREADS {
             let job_recv = job_recv.clone();
-
-            s.spawn(move || {
-                println!("starting worker {}", i);
-                while let Ok(job) = job_recv.recv() {
-                    println!("worker {} transcoding from {} to {}", i,
-                        job.src.display(), job.dest.display());
-                }
-                println!("worker {} is done", i);
-            });
+            s.spawn(move || transcoder(i, job_recv));
         }
+
+        jobscheduler(path, job_sender);
+    });
+
+    println!("done");
+}
+
+fn jobscheduler(path: PathBuf, job_sender: crossbeam_channel::Sender<TranscodeJob>) {
         println!("starting job scheduler");
+
+        let entries = fs::read_dir(path).unwrap();
 
         let job = TranscodeJob{
             src: PathBuf::from("src"),
@@ -44,7 +47,15 @@ fn main() {
         };
         job_sender.send(job).expect("failed to send job to workers");
         drop(job_sender);
-    });
+}
 
-    println!("done");
+fn transcoder(id: i32, job_recv: crossbeam_channel::Receiver<TranscodeJob>) {
+    println!("starting transcoder {}", id);
+
+    while let Ok(job) = job_recv.recv() {
+        println!("transcoder {} transcoding from {} to {}", id,
+            job.src.display(), job.dest.display());
+    }
+
+    println!("finishing transcoder {}", id);
 }
