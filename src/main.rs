327245541143e02c4de050e079172fa89bdf9256
Tiago Katcipis <tiagokatcipis@gmail.com>
2022-10-16 17:24:19 +0200

    feat: advance on listing and filtering files per extension

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use std::thread;
-use std::path::PathBuf;
-use crossbeam_channel;
+use crossbeam_channel::bounded;
+use crossbeam_channel::Receiver;
+use crossbeam_channel::Sender;
 use std::env;
 use std::fs;
+use std::path::{Path, PathBuf};
+use std::thread;
+use std::vec::Vec;
 
 #[derive(Debug)]
 struct TranscodeJob {
@@ -15,46 +18,76 @@ fn main() {
 
     let args: Vec<String> = env::args().collect();
 
-    let path = match args.len() {
+    let pathbuf = match args.len() {
         2 => PathBuf::from(&args[1]),
         _ => env::current_dir().unwrap(),
     };
 
-    println!("looking for videos at: {}", path.display());
+    println!("looking for videos at: {}", pathbuf.display());
 
     thread::scope(|s| {
-        let (job_sender, job_recv) = crossbeam_channel::bounded::<TranscodeJob>(0);
+        let (job_sender, job_recv) = bounded::<TranscodeJob>(0);
 
         for i in 0..NTHREADS {
             let job_recv = job_recv.clone();
             s.spawn(move || transcoder(i, job_recv));
         }
 
-        jobscheduler(path, job_sender);
+        jobscheduler(pathbuf.as_path(), job_sender);
     });
 
     println!("done");
 }
 
-fn jobscheduler(path: PathBuf, job_sender: crossbeam_channel::Sender<TranscodeJob>) {
-        println!("starting job scheduler");
+fn jobscheduler(path: &Path, job_sender: Sender<TranscodeJob>) {
+    println!("starting job scheduler");
 
-        let entries = fs::read_dir(path).unwrap();
+    let entries = fs::read_dir(path).unwrap();
+    let mut srcs = Vec::new();
 
-        let job = TranscodeJob{
-            src: PathBuf::from("src"),
-            dest: PathBuf::from("dest"),
-        };
-        job_sender.send(job).expect("failed to send job to workers");
-        drop(job_sender);
+    for entry in entries {
+        match entry {
+            Ok(entry) => {
+                let path = entry.path();
+                if let Some(extension) = path.extension() {
+                    let extension = extension
+                        .to_str()
+                        .expect("file extensions should be valid utf-8");
+
+                    match extension {
+                        "avi" => {
+                            srcs.push(path);
+                        }
+                        _ => {
+                            println!("ignoring file with extension: {}", extension)
+                        }
+                    }
+                }
+            }
+            Err(err) => {
+                print!("error reading dir {}: {}", path.display(), err);
+            }
+        }
+    }
+
+    let job = TranscodeJob {
+        src: PathBuf::from("src"),
+        dest: PathBuf::from("dest"),
+    };
+    job_sender.send(job).expect("failed to send job to workers");
+    drop(job_sender);
 }
 
-fn transcoder(id: i32, job_recv: crossbeam_channel::Receiver<TranscodeJob>) {
+fn transcoder(id: i32, job_recv: Receiver<TranscodeJob>) {
     println!("starting transcoder {}", id);
 
     while let Ok(job) = job_recv.recv() {
-        println!("transcoder {} transcoding from {} to {}", id,
-            job.src.display(), job.dest.display());
+        println!(
+            "transcoder {} transcoding from {} to {}",
+            id,
+            job.src.display(),
+            job.dest.display()
+        );
     }
 
     println!("finishing transcoder {}", id);
