aadde4816053477d803ce819b09e48f2b3506aad
Tiago Katcipis <tiagokatcipis@gmail.com>
2022-10-29 20:37:02 +0200

    feat: finish dest/src dirs logic

--- src/main.rs
+++ src/main.rs
@@ -14,29 +14,19 @@ struct TranscodeJob {
     dest: PathBuf,
 }
 
-fn main() -> ExitCode {
-    let args: Vec<String> = env::args().collect();
-
-    let (srcdir, destdir) = match args.len() {
-        1 => {
-            let src = env::current_dir().unwrap();
-            let mut dest = src.clone();
-            dest.push("transcoded");
-            (src, dest)
-        }
-        3 => (PathBuf::from(&args[1]), PathBuf::from(&args[2])),
-        _ => {
-            println!("usage using current dir: {}", args[0]);
-            println!("usage explicit dirs: {} <src dir> <dest dir>", args[0]);
-            std::process::exit(1)
-        }
-    };
+fn main() -> Result<(), ExitCode> {
+    let (srcdir, destdir) = parse_args()?;
 
     println!(
         "videos src: {} dest: {}",
         srcdir.display(),
         destdir.display()
     );
+
+    if srcdir == destdir {
+        println!("src and dest dirs must differ");
+        return Err(ExitCode::FAILURE);
+    }
 
     thread::scope(|s| {
         const NTHREADS: i32 = 4;
@@ -48,32 +38,32 @@ fn main() -> ExitCode {
             s.spawn(move || transcoder(i, job_recv));
         }
 
-        jobscheduler(srcdir.as_path(), job_sender);
+        jobscheduler(srcdir.as_path(), destdir.as_path(), job_sender);
     });
 
     println!("done");
 
-    ExitCode::SUCCESS
+    Ok(())
 }
 
-fn jobscheduler(path: &Path, job_sender: Sender<TranscodeJob>) {
+fn jobscheduler(srcdir: &Path, destdir: &Path, job_sender: Sender<TranscodeJob>) {
     println!("starting job scheduler");
 
-    let entries = fs::read_dir(path).unwrap();
+    let entries = fs::read_dir(srcdir).unwrap();
     let mut srcs = Vec::new();
 
     for entry in entries {
         match entry {
             Ok(entry) => {
-                let path = entry.path();
-                if let Some(extension) = path.extension() {
+                let srcdir = entry.path();
+                if let Some(extension) = srcdir.extension() {
                     let extension = extension
                         .to_str()
                         .expect("file extensions should be valid utf-8");
 
                     match extension {
                         "avi" => {
-                            srcs.push(path);
+                            srcs.push(srcdir);
                         }
                         _ => {
                             println!("ignoring file with extension: {}", extension)
@@ -82,16 +72,23 @@ fn jobscheduler(path: &Path, job_sender: Sender<TranscodeJob>) {
                 }
             }
             Err(err) => {
-                print!("error reading dir {}: {}", path.display(), err);
+                print!("error reading dir {}: {}", srcdir.display(), err);
             }
         }
     }
 
-    let job = TranscodeJob {
-        src: PathBuf::from("src"),
-        dest: PathBuf::from("dest"),
-    };
-    job_sender.send(job).expect("failed to send job to workers");
+    for src in srcs {
+        let mut dest = destdir.to_path_buf();
+        // We already validated srcs, so they always have a filename
+        dest.push(src.file_name().unwrap());
+
+        let job = TranscodeJob {
+            src: src,
+            dest: dest,
+        };
+        job_sender.send(job).expect("failed to send job to workers");
+    }
+
     drop(job_sender);
 }
 
@@ -108,4 +105,23 @@ fn transcoder(id: i32, job_recv: Receiver<TranscodeJob>) {
     }
 
     println!("finishing transcoder {}", id);
+}
+
+fn parse_args() -> Result<(PathBuf, PathBuf), ExitCode> {
+    let args: Vec<String> = env::args().collect();
+
+    return match args.len() {
+        1 => {
+            let src = env::current_dir().unwrap();
+            let mut dest = src.clone();
+            dest.push("transcoded");
+            Ok((src, dest))
+        }
+        3 => Ok((PathBuf::from(&args[1]), PathBuf::from(&args[2]))),
+        _ => {
+            println!("usage using current dir: {}", args[0]);
+            println!("usage explicit dirs: {} <src dir> <dest dir>", args[0]);
+            Err(ExitCode::FAILURE)
+        }
+    };
 }
